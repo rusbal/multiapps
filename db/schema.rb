468f3b52d100d1fa0e6646c39e89afed60905612
Raymond Usbal <raymond@philippinedev.com>
2025-01-19 23:19:53 +0800

    scaffold: users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2025_01_19_151808) do
+ActiveRecord::Schema[7.0].define(version: 2025_01_19_151940) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -24,6 +24,14 @@ ActiveRecord::Schema[7.0].define(version: 2025_01_19_151808) do
   create_table "transactions", force: :cascade do |t|
     t.string "title"
     t.integer "amount"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "position"
+    t.integer "age"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
